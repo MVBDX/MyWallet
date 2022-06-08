76e4036d4d870e5c81d5355f2ce12eea9bab80a9
m.faalian <m.faalian@behpardaz.net>
2022-06-08 15:23:58 +0430

    feat security config

---
+++ src/main/java/ir/mvbdx/mywallet/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package ir.mvbdx.mywallet.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement()
+                .maximumSessions(1)
+                .expiredUrl("/expired")
+                .maxSessionsPreventsLogin(false)
+                .and()
+
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .invalidSessionUrl("/invalid")
+                .and()
+
+                .httpBasic()
+                .and()
+
+                .authorizeRequests()
+                .antMatchers("/welcome").permitAll()
+                .and()
+
+                .authorizeRequests()
+//                .antMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                .antMatchers("/api/users/").hasAnyRole("ADMIN", "MANAGER", "USER")
+                .antMatchers("/api/users/new").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/api/users/edit/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/api/users/**", "/api/user/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+
+                .formLogin().permitAll()
+                .defaultSuccessUrl("/index", true)
+                .and()
+
+                .logout().permitAll()
+                .logoutUrl("/logout")
+                .and()
+
+                .csrf().disable()
+        ;
+    }
+}
