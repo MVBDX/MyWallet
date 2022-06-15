a5f695e794d4b2639126cd54866acc7c3659824c
m.faalian <m.faalian@behpardaz.net>
2022-06-15 09:22:03 +0430

    feat index

--- src/main/java/ir/mvbdx/mywallet/controller/HomeController.java
+++ src/main/java/ir/mvbdx/mywallet/controller/HomeController.java
@@ -1,11 +1,16 @@
 package ir.mvbdx.mywallet.controller;
 
 import ir.mvbdx.mywallet.entity.Category;
+import ir.mvbdx.mywallet.entity.Customer;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -13,6 +18,20 @@ public class HomeController {
     @GetMapping("/index")
     public String index() {
         return "index";
+    }
+
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("customerForm", new Customer());
+        List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
+        model.addAttribute("listProfession", listProfession);
+        return "register";
+    }
+
+    @PostMapping("/result")
+    public String submissionResult(@ModelAttribute("customerForm") Customer customer) {
+        return "result";
     }
 
     @PostMapping("/save")
