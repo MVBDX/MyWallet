0d20995271d55a66993dfaf7e095020664c2d2e1
m.faalian <m.faalian@behpardaz.net>
2022-06-15 09:30:15 +0430

    feat transaction amount apply into account

--- src/main/java/ir/mvbdx/mywallet/service/impl/TransactionServiceImpl.java
+++ src/main/java/ir/mvbdx/mywallet/service/impl/TransactionServiceImpl.java
@@ -1,6 +1,8 @@
 package ir.mvbdx.mywallet.service.impl;
 
+import ir.mvbdx.mywallet.entity.Account;
 import ir.mvbdx.mywallet.entity.Transaction;
+import ir.mvbdx.mywallet.entity.TransactionType;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
@@ -9,5 +11,16 @@ import org.springframework.stereotype.Service;
 public class TransactionServiceImpl extends BaseServiceImpl<Transaction> {
     public TransactionServiceImpl(@Qualifier("transactionRepository") JpaRepository<Transaction, Long> baseRepository) {
         super(baseRepository, "Transaction");
+    }
+
+    @Override
+    public Transaction save(Transaction base) {
+        Account account = base.getAccount();
+        if (base.getType().equals(TransactionType.EXPENSE))
+            account.setBalance(account.getBalance() - base.getAmount());
+        else if (base.getType().equals(TransactionType.INCOME))
+            account.setBalance(account.getBalance() + base.getAmount());
+        // todo implement transfer between two account
+        return super.save(base);
     }
 }
