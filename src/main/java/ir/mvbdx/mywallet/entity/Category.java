691545860a48b10bdd369deb356b15f78bbb2f56
m.faalian <m.faalian@behpardaz.net>
2022-06-18 12:44:28 +0430

    feat edit and delete button

--- src/main/java/ir/mvbdx/mywallet/entity/Category.java
+++ src/main/java/ir/mvbdx/mywallet/entity/Category.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
+@SQLDelete(sql = "UPDATE category SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,9 +22,10 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @OneToMany (mappedBy = "category")
+    @OneToMany(mappedBy = "category")
     private Set<Transaction> transactions;
     @ManyToOne
     @JoinColumn(name = "parent_id")
     private Category parentCategory;
+    private boolean deleted = Boolean.FALSE;
 }
