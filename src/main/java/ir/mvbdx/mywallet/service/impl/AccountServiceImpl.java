3b040a2d2ffb32b24284f9b3b71f6154cdc44208
m.faalian <m.faalian@behpardaz.net>
2022-06-19 16:40:16 +0430

    feat total (income/outcome) for account service

--- src/main/java/ir/mvbdx/mywallet/service/impl/AccountServiceImpl.java
+++ src/main/java/ir/mvbdx/mywallet/service/impl/AccountServiceImpl.java
@@ -1,13 +1,35 @@
 package ir.mvbdx.mywallet.service.impl;
 
 import ir.mvbdx.mywallet.entity.Account;
+import ir.mvbdx.mywallet.repository.AccountRepository;
+import ir.mvbdx.mywallet.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
+import java.util.Optional;
+
 @Service
 public class AccountServiceImpl extends BaseServiceImpl<Account> {
+    @Autowired
+    private TransactionRepository transactionRepository;
+    private AccountRepository accountRepository;
+
     public AccountServiceImpl(@Qualifier("accountRepository") JpaRepository<Account, Long> baseRepository) {
         super(baseRepository, "Account");
+        accountRepository = (AccountRepository) baseRepository;
     }
+
+    public String totalIncome(Long accountId) {
+        Optional<Account> account = accountRepository.findById(accountId);
+        return new DecimalFormat("#").format(transactionRepository.totalIncomeOfAccount(account.get().getId()));
+    }
+
+    public Double totalOutcome(Long accountId) {
+        Optional<Account> account = accountRepository.findById(accountId);
+        return transactionRepository.totalSpendOfAccount(account.get().getId());
+    }
+
 }
