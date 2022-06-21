b2b887bcedf5135aad5a173a82ae8c21b2975c7b
m.faalian <m.faalian@behpardaz.net>
2022-06-21 09:29:01 +0430

    feat delete: to prevent broken relation

--- src/main/java/ir/mvbdx/mywallet/service/impl/CategoryServiceImpl.java
+++ src/main/java/ir/mvbdx/mywallet/service/impl/CategoryServiceImpl.java
@@ -1,13 +1,32 @@
 package ir.mvbdx.mywallet.service.impl;
 
 import ir.mvbdx.mywallet.entity.Category;
+import ir.mvbdx.mywallet.exception.EntityHaveRelationException;
+import ir.mvbdx.mywallet.exception.EntityNotFoundException;
+import ir.mvbdx.mywallet.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CategoryServiceImpl extends BaseServiceImpl<Category> {
+    private CategoryRepository categoryRepository;
+
     public CategoryServiceImpl(@Qualifier("categoryRepository") JpaRepository<Category, Long> baseRepository) {
         super(baseRepository, "Category");
+        categoryRepository = (CategoryRepository) baseRepository;
+    }
+
+    @Override
+    public void delete(Long id) {
+        Optional<Category> base = categoryRepository.findById(id);
+        if (base.isEmpty())
+            throw new EntityNotFoundException(id, entityName);
+        if (base.get().getTransactions().isEmpty() && base.get().getSubordinates().isEmpty())
+            baseRepository.deleteById(id);
+        else
+            throw new EntityHaveRelationException(entityName + " " + base.get().getName());
     }
 }
