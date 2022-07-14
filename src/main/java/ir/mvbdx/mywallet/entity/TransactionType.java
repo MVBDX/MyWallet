046a75b2df46b4f9112556414186373f796bbf8b
MVBDX <faalian.m@gmail.com>
2022-07-14 12:48:22 +0430

    feat display name

--- src/main/java/ir/mvbdx/mywallet/entity/TransactionType.java
+++ src/main/java/ir/mvbdx/mywallet/entity/TransactionType.java
@@ -1,5 +1,17 @@
 package ir.mvbdx.mywallet.entity;
 
 public enum TransactionType {
-    EXPENSE, INCOME, TRANSFER
+    EXPENSE("Expense"),
+    INCOME("Income"),
+    TRANSFER("Transfer");
+
+    private final String displayValue;
+
+    private TransactionType(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
