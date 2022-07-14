27f998a515e12384efced3169a3d0cd95bfb0750
MVBDX <faalian.m@gmail.com>
2022-07-14 19:02:17 +0430

    fix @Where in entity handle 'deleted' clause

--- src/main/java/ir/mvbdx/mywallet/repository/AccountRepository.java
+++ src/main/java/ir/mvbdx/mywallet/repository/AccountRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    @Query("SELECT SUM(a.balance) FROM Account AS a WHERE a.deleted = false ")
+    @Query("SELECT SUM(balance) FROM Account")
     Double totalBalance();
 
 }
