c7d62231f3cd5a02b8b815e691d2456ccacad4e7
MVBDX <faalian.m@gmail.com>
2022-07-18 19:47:25 +0430

    feat deposit/withdraw functions

--- src/main/java/ir/mvbdx/mywallet/entity/Account.java
+++ src/main/java/ir/mvbdx/mywallet/entity/Account.java
@@ -1,6 +1,7 @@
 package ir.mvbdx.mywallet.entity;
 
 import ir.mvbdx.mywallet.enumeration.AccountType;
+import ir.mvbdx.mywallet.exception.AccountException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,7 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@SQLDelete(sql = "UPDATE account SET deleted = true WHERE id=?")
+@SQLDelete(sql = "UPDATE account SET deleted = true WHERE id = ?")
 @Where(clause = "deleted=false")
 @Getter
 @Setter
@@ -31,4 +32,15 @@ public class Account {
     @OneToMany(mappedBy = "account")
     private Set<Transaction> transactions;
     private boolean deleted = Boolean.FALSE;
+
+    public void deposit(Double amount) {
+        this.setBalance(this.getBalance() + amount);
+    }
+
+    public void withdraw(Double amount) {
+        if (this.getBalance() >= amount) {
+            this.setBalance(this.getBalance() - amount);
+        } else throw new AccountException("Account balance is not enough!");
+    }
+
 }
