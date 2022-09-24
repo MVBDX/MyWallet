02afd680a445f4716283a02be10ba681ae43601a
MVBDX <faalian.m@gmail.com>
2022-09-24 23:25:44 +0330

    refactor move beans to myconfig class

---
+++ src/main/java/ir/mvbdx/mywallet/config/MyConfiguration.java
@@ -0,0 +1,19 @@
+package ir.mvbdx.mywallet.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class MyConfiguration {
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new CustomAuditAware();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
