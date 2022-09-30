c30d30dda86220d1d3f2aa095f394ead546c4918
MVBDX <faalian.m@gmail.com>
2022-09-30 15:23:10 +0330

    feat rest controller for account

---
+++ src/main/java/ir/mvbdx/mywallet/controller/api/AccountApiController.java
@@ -0,0 +1,59 @@
+package ir.mvbdx.mywallet.controller.api;
+
+import ir.mvbdx.mywallet.dto.AccountDTO;
+import ir.mvbdx.mywallet.dto.CustomerDTO;
+import ir.mvbdx.mywallet.entity.Account;
+import ir.mvbdx.mywallet.service.AccountService;
+import ir.mvbdx.mywallet.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/account")
+@RequiredArgsConstructor
+public class
+AccountApiController {
+    private final AccountService accountService;
+    private final CustomerService customerService;
+    private final ModelMapper modelMapper;
+
+    @GetMapping({"/list", "/"})
+//    @ResponseStatus(HttpStatus.OK)
+    public List<AccountDTO> findAll() {
+        return accountService.findAll().stream()
+                .map(account -> modelMapper.map(account, AccountDTO.class)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public AccountDTO find(@PathVariable("id") Long id) {
+        return modelMapper.map(accountService.findById(id), AccountDTO.class);
+    }
+
+    @PostMapping("/save")
+    public Account save(@Valid @RequestBody AccountDTO accountDTO, Principal principal) {
+        accountDTO.setCustomer(modelMapper.map(customerService.findCustomer(principal), CustomerDTO.class));
+        return accountService.save(modelMapper.map(accountDTO, Account.class));
+    }
+
+    @PutMapping("/edit")
+    public Account update(@Valid @RequestBody AccountDTO accountDTO, Principal principal) {
+        accountDTO.setCustomer(modelMapper.map(customerService.findCustomer(principal), CustomerDTO.class));
+        return accountService.update(accountDTO.getId(), modelMapper.map(accountDTO, Account.class));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Long> deleteApi(@PathVariable("id") Long id) {
+        if (!accountService.delete(id))
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+}
