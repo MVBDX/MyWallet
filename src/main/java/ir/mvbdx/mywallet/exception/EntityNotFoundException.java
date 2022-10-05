50e5bad9f7b974d6223f1034b167313eabac6167
MVBDX <faalian.m@gmail.com>
2022-10-05 22:07:42 +0330

    refactor exception message with string format

--- src/main/java/ir/mvbdx/mywallet/exception/EntityNotFoundException.java
+++ src/main/java/ir/mvbdx/mywallet/exception/EntityNotFoundException.java
@@ -2,6 +2,6 @@ package ir.mvbdx.mywallet.exception;
 
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(Long id, String entity) {
-        super(entity + " id not found: " + id);
+        super(String.format("%s with Id %d not found.", entity, id));
     }
 }
