5988b025e32b6b6be1e13fe7fce17acfe483770c
MVBDX <faalian.m@gmail.com>
2022-11-10 21:24:16 +0330

    refactor naming

--- src/main/java/ir/mvbdx/mywallet/model/enums/CustomerStatus.java
+++ src/main/java/ir/mvbdx/mywallet/model/enums/CustomerStatus.java
@@ -1,8 +1,5 @@
 package ir.mvbdx.mywallet.model.enums;
 
 public enum CustomerStatus {
-    ACTIVATE,
-    DEACTIVATE,
-    AWAITING_APPROVAL,
-    REJECTED
+    REGISTERED, ANONYMOUS, PENDING, BLOCKED
 }
